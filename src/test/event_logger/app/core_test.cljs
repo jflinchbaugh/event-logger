c9f1c66ce3fb0f92700714a12b6050b822fe8b6d
John Flinchbaugh <john@hjsoft.com>
2023-01-25 14:41:26 -0500

    clean out old tests

--- src/test/event_logger/app/core_test.cljs
+++ src/test/event_logger/app/core_test.cljs
@@ -3,20 +3,6 @@
             [reagent.core :as r]
             [event-logger.app.core :as sut]))
 
-
-#_(deftest increment-atom
-  (testing "increases the atom value by one"
-    (let [r (r/atom 0)
-          _ (incrementer! r)]
-      (is (= 1 @r)))))
-
-#_(deftest decrement-atom
-  (testing "decreases the atom value by one"
-    (let [r (r/atom 0)
-          _ (decrementer! r)]
-      (is (= -1 @r)))))
-
-
 (t/deftest test-make-id
   (t/is (= "my-id" (sut/make-id "My ID"))))
 
