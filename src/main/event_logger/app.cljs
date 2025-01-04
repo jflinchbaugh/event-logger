98e35753a4bd2e6fafe9f0c37c55bc971a8d22a6
John Flinchbaugh <john@hjsoft.com>
2025-01-04 13:12:27 -0500

    switching over to helix

--- src/main/event_logger/app.cljs
+++ src/main/event_logger/app.cljs
@@ -5,6 +5,8 @@
             ["react-dom/client" :as rdom]
             [tick.core :as t]))
 
+;; utilities
+
 (defn format-date-time [dt]
   (t/format :iso-local-date-time dt))
 
@@ -12,27 +14,6 @@
   "produce a string for the datetime now"
   []
   (-> (t/date-time) (t/truncate :seconds) format-date-time))
-
-(defn is-new-event? [existing-events event]
-  (empty? (filter (partial = event) existing-events)))
-
-(defn add-event! [state set-state id]
-  (if (nil? (:adding-event state))
-    (set-state assoc :adding-event (now-str))
-    (let [time (:adding-event state)
-          idx (.indexOf (map :id (:categories state)) id)
-          existing-events (get-in state [:categories idx :events])]
-      (when (is-new-event? existing-events time)
-        (print "adding" time "to" id "at" idx)
-        (set-state update-in [:categories idx :events] conj time)
-        (set-state assoc :adding-event nil)))))
-
-;; define components using the `defnc` macro
-
-(defnc add-button [{:keys [state set-state item display]}]
-  (d/button
-    {:on-click #(add-event! state set-state (:id item))}
-    display))
 
 (defn normalize-date-str
   "parse, truncate, and reformat a date string"
@@ -42,6 +23,37 @@
     t/date-time
     (t/truncate :seconds)
     format-date-time))
+
+(defn is-new-event? [existing-events event]
+  (empty? (filter (partial = event) existing-events)))
+
+(defn have-event? [state]
+  (-> state :adding-event nil?))
+
+;; action
+
+(defn add-event! [state set-state id]
+  (if (have-event? state)
+  (set-state assoc :adding-event (now-str))
+  (let [time (:adding-event state)
+        idx (.indexOf (map :id (:categories state)) id)
+        existing-events (get-in state [:categories idx :events])]
+    (when (is-new-event? existing-events time)
+      (print "adding" time "to" id "at" idx)
+      (set-state update-in [:categories idx :events] conj time)
+      (set-state assoc :adding-event nil)))))
+
+(defn open-category! [state set-state item]
+  (when (not= (:id  item) (:display-category state))
+    (set-state assoc :adding-event nil)
+    (set-state assoc :display-category (:id item))))
+
+;; define components using the `defnc` macro
+
+(defnc add-button [{:keys [state set-state item display]}]
+(d/button
+  {:on-click (partial add-event! state set-state (:id item))}
+    display))
 
 (defnc category-details [{:keys [set-state state item]}]
   (d/div {:class "details" :id (str "details-" (:id item))}
@@ -73,8 +85,8 @@
       (doall
         (for [item categories]
           (d/li
-            {:on-click #(set-state assoc :display-category (:id item))
-            :key (:id item)}
+            {:on-click (partial open-category! state set-state item)
+             :key (:id item)}
             ($ add-button {:state state :set-state set-state :item item :display "+"})
             (d/label (:name item))
             (when (= (:id item) (:display-category state))
