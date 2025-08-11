9618ab150c9404c5100b2540fafca945cf57a766
John Flinchbaugh <john@hjsoft.com>
2025-08-11 12:08:13 -0400

    add service worker for offline caching of the app

--- src/main/event_logger/app.cljs
+++ src/main/event_logger/app.cljs
@@ -7,12 +7,32 @@
 ;; start your app with your favorite React render
 (defonce root (rdom/createRoot (js/document.getElementById "root")))
 
+(defn https? [location]
+  (= (.-protocol location) "https:"))
+
+(defn register-service-worker []
+  (when (and js/ServiceWorker (https? js/location))
+    (.addEventListener js/window "load"
+      (fn []
+        (-> (.-serviceWorker js/navigator)
+          (.register "service-worker.js")
+          (.then (fn [registration]
+                   (tel/log!
+                     :info
+                     (str
+                       "ServiceWorker registration successful with scope:"
+                       (.-scope registration)))))
+          (.catch (fn [err]
+                    (tel/log!
+                      :error
+                      (str "ServiceWorker registration failed:" err)))))))))
 (defn render []
   (tel/log! :info "rendering app")
   (.render root ($ app)))
 
 (defn ^:export init []
-  (render))
+  (render)
+  (register-service-worker))
 
 (defn ^:dev/after-load reload! []
   (render))
